74b8d6f8c74f0fed54e8828d5c96b4195be0ef96
Jagjit Singh Ramgarhia <49023172+jonjonnz@users.noreply.github.com>
2020-09-01 19:44:10 +0530

    added tasks Added code for input box changed network for handling new/existing game

--- client.py
+++ client.py
@@ -3,29 +3,52 @@ from network import Network
 import pygbutton
 import os
 
-# Default Screen size
-width = 800
-height = 600
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 32)
 
-# Folder Structures for images
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, "images")
-bg_img_folder = os.path.join(img_folder, "background")
 
-# Load images
-background_img = pygame.image.load(os.path.join(bg_img_folder, "mainscreen.jpg"))
-background_img = pygame.transform.scale(background_img, (width, height))
+class InputBox:
 
-# Button Defaults
-button_row = 465
-button_column = 100
-button_color = (0, 200, 0)
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
 
-# Initialization and creating a window
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("MPG-Test")
-clock = pygame.time.Clock()
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width() + 10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
 
 
 def draw_main_screen(s, buttons):
@@ -48,22 +71,76 @@ def draw_text(surf, text, size, x, y, color):
 
 def main_screen():
     """Main screen loop"""
-
-    while True:
+    # Buttons for game to connect to a new game or an existing one
+    new_game_button = pygbutton.PygButton((button_column, button_row, 100, 50), 'New Game', button_color)
+    existing_game_button = pygbutton.PygButton((button_column, button_row + 75, 100, 50), 'Join Existing', button_color)
+    quit_game_button = pygbutton.PygButton((button_column, button_row + 150, 100, 50), 'Quit', button_color)
+    buttons = {'new': new_game_button, 'existing': existing_game_button, 'quit': quit_game_button}
+    run = True
+    while run:
         clock.tick(60)
-
-        # Buttons for game to connect to a new game or an existing one
-        new_game_button = pygbutton.PygButton((button_column, button_row, 100, 50), 'New Game', button_color)
-        existing_game_button = pygbutton.PygButton((button_column + 200, button_row, 100, 50), 'Join Existing', button_color)
-        quit_game_button = pygbutton.PygButton((button_column + 400, button_row, 100, 50), 'Quit', button_color)
-        buttons = [new_game_button, existing_game_button, quit_game_button]
-
         # Checking for event on the main screen
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                run = False
+            if 'click' in buttons['new'].handleEvent(event):
+                start_new_game()
+            elif 'click' in buttons['existing'].handleEvent(event):
+                join_existing_game()
+            elif 'click' in buttons['quit'].handleEvent(event):
+                run = False
 
-        draw_main_screen(screen, buttons)
+        draw_main_screen(screen, buttons.values())
+    pygame.quit()
 
+
+# Default Screen size
+width = 800
+height = 600
+
+# Folder Structures for images
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "images")
+bg_img_folder = os.path.join(img_folder, "background")
+
+# Load images
+background_img = pygame.image.load(os.path.join(bg_img_folder, "mainscreen.jpg"))
+background_img = pygame.transform.scale(background_img, (width, height))
+
+# Button Defaults
+button_row = 100
+button_column = 100
+button_color = (0, 200, 0)
+
+# Initialization and creating a window
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("MPG-Test")
+clock = pygame.time.Clock()
 
 main_screen()
+
+
+# def main():
+#     clock = pygame.time.Clock()
+#     input_box1 = InputBox(100, 100, 140, 32)
+#     input_box2 = InputBox(100, 300, 140, 32)
+#     input_boxes = [input_box1, input_box2]
+#     done = False
+#
+#     while not done:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 done = True
+#             for box in input_boxes:
+#                 box.handle_event(event)
+#
+#         for box in input_boxes:
+#             box.update()
+#
+#         screen.fill((30, 30, 30))
+#         for box in input_boxes:
+#             box.draw(screen)
+#
+#         pygame.display.flip()
+#         clock.tick(30)
--- network.py
+++ network.py
@@ -8,23 +8,29 @@ class Network:
         self.server = "192.168.0.102"  # IP of the server we need to connect
         self.port = 5555  # Port of the server
         self.address = (self.server, self.port)  # Combined address of the server
-        self.player = self.connect()
-
-    def get_client(self):
-        return self.player
+        # self.player = self.connect()
 
     def connect(self):
         try:
-            # Connecting to address and receiving back object from the server
             self.client.connect(self.address)
-            return pickle.loads(self.client.recv(2048))
         except Exception as err:
             print(err)
-            pass
 
-    def send(self, data):
-        try:  # Sending the server an object and receiving something back
-            self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(2048))
-        except Exception as err:
-            print(err)
+    # def get_client(self):
+    #     return self.player
+    #
+    # def connect(self):
+    #     try:
+    #         # Connecting to address and receiving back object from the server
+    #         self.client.connect(self.address)
+    #         return pickle.loads(self.client.recv(2048))
+    #     except Exception as err:
+    #         print(err)
+    #         pass
+    #
+    # def send(self, data):
+    #     try:  # Sending the server an object and receiving something back
+    #         self.client.send(pickle.dumps(data))
+    #         return pickle.loads(self.client.recv(2048))
+    #     except Exception as err:
+    #         print(err)
